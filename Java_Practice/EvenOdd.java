c0d1d8df3926890347bc47a5b0c3018fae0428aa
raghu <raghumina11@gmail.com>
2020-05-12 08:13:33 -0700

    Java Practice 1

---
+++ Java_Practice/EvenOdd.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+class EvenOdd {
+
+    public static void evenOdd() {
+        System.out.println("PLEASE ENTER S NUMBER");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        if(i/2==0){
+            System.out.println("Number is Even ");
+        }
+        else{
+            System.out.println("Number is Odd");
+        }
+    }
+}
